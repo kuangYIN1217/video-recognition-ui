6266d0f7ebcdf808da3ebcb116c2610f8d7c8774
kuangYIN1217 <519997474@qq.com>
2017-11-27 09:48:14 +0800

    'merge'

--- src/app/taskmanage/taskmanage.component.ts
+++ src/app/taskmanage/taskmanage.component.ts
@@ -29,31 +29,22 @@ export class TaskManageComponent {
   deleteIdArr:any[]=[];
   percent:any[]=[];
   interval: any;
-  interval1: any;
-  percentProgress:number=0.00;
-  interval10: any;
   pageNow:number;
   playShow:boolean=false;
   videoBtn:number=2;
   showBtn:boolean=false;
-  showBtn1:boolean=false;
-  showBtn2:boolean=false;
-  showBtn3:boolean=false;
   outputPath:string;
   nameTem:string;
   @ViewChild('offlineVideo') offlineVideo: any;
-  @ViewChild('offlineVideo1') offlineVideo1: any;
-  @ViewChild('offlineVideo2') offlineVideo2: any;
-  @ViewChild('offlineVideo3') offlineVideo3: any;
   constructor(private offlineService:OfflineService, private route: ActivatedRoute ,private router: Router,private websocket: WebSocketService) {
     this.appId = window.sessionStorage.getItem("applicationId");
     this.appCate = window.sessionStorage.getItem("applicationType");
     //console.log(this.appId);
     //console.log(this.appCate);
     this.getTask(this.appId,null,'全部',this.page-1,this.pageMaxItem);
-/*    this.interval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.getTask(this.appId,null,'全部',this.page-1,this.pageMaxItem);
-    }, 10000);*/
+    }, 10000);
     this.alarmStatus = this.alarmStatusArr[0];
     this.route.params.subscribe((param) => {
       if(JSON.stringify(param) != "{}"){
@@ -102,22 +93,20 @@ export class TaskManageComponent {
       })
   }
   lookResult(item){
-    if(item.percent!=100.00){
-      return false;
-    }
     this.playShow=true;
-    // this.outputPath = item.outputPath;
-    this.nameTem = item.taskName;
-    console.log(this.nameTem);
+    this.outputPath = item.outputPath;
+    console.log(this.outputPath);
   }
   closeVideo(){
     this.playShow=false;
+    this.showBtn = false;
   }
   show(){
     this.videoBtn = 1;
   }
   hide(){
     this.videoBtn = 2;
+    this.showBtn = false;
   }
   play() {
     if (this.offlineVideo.nativeElement.paused) {
@@ -128,33 +117,6 @@ export class TaskManageComponent {
       this.showBtn = false;
     }
 
-  }
-  play1() {
-    if (this.offlineVideo1.nativeElement.paused) {
-      this.offlineVideo1.nativeElement.play();
-      this.showBtn1 = true;
-    } else {
-      this.offlineVideo1.nativeElement.pause();
-      this.showBtn1 = false;
-    }
-  }
-  play2() {
-    if (this.offlineVideo2.nativeElement.paused) {
-      this.offlineVideo2.nativeElement.play();
-      this.showBtn2 = true;
-    } else {
-      this.offlineVideo2.nativeElement.pause();
-      this.showBtn2 = false;
-    }
-  }
-  play3() {
-    if (this.offlineVideo3.nativeElement.paused) {
-      this.offlineVideo3.nativeElement.play();
-      this.showBtn3 = true;
-    } else {
-      this.offlineVideo3.nativeElement.pause();
-      this.showBtn3 = false;
-    }
   }
   running(item){
     console.log(item);
@@ -220,47 +182,12 @@ export class TaskManageComponent {
   }
   runChannel(item){
     if(item.taskStatus!='进行中'){
-      //this.status='进行中';
-      item.taskStatus = '进行中';
-      //this.alarmStatus = '全部';
-      this.interval10 = setInterval(() =>{
-        item.percent=item.percent+Math.round(Math.random()*5+10);
-        item.taskStatus = '进行中';
-        if(item.percent>=90){
-          item.percent = 100.00;
-          item.taskStatus='完成';
-          let obj:any={};
-          obj.name = item.taskName;
-          obj.percent = item.percent;
-          obj.taskStatus = item.taskStatus;
-          let a:string;
-          a=item.taskName;
-          clearInterval(this.interval10);
-          console.log(obj);
-          if(a=='特定人识别-吴京'){
-            sessionStorage.setItem("特定人识别-吴京",JSON.stringify(obj));
-          }
-          if(a=='威胁度识别-枪械'){
-            sessionStorage.setItem("威胁度识别-枪械",JSON.stringify(obj));
-          }
-          if(a=='作战目标识别-车+直升机'){
-            sessionStorage.setItem("作战目标识别-车+直升机",JSON.stringify(obj));
-          }
-          if(a=='作战目标识别-车+直升机+坦克'){
-            sessionStorage.setItem("作战目标识别-车+直升机+坦克",JSON.stringify(obj));
-          }
-        }
-
-      },2000);
+      this.status='进行中';
+      this.alarmStatus = '全部';
     }else if(item.taskStatus=='进行中'){
-      //this.status='暂停';
-      item.taskStatus=='暂停';
-      clearInterval(this.interval10);
+      this.status='暂停';
     }
-    if(item.percent==100.00){
-      item.taskStatus='完成'
-    }
-/*    this.offlineService.offlineSwitch(item.taskId,this.status)
+    this.offlineService.offlineSwitch(item.taskId,this.status)
       .subscribe(reply =>{
         if(reply.text().substring(0,2)=='No'){
           this.deleteIndex = 1;
@@ -291,7 +218,7 @@ export class TaskManageComponent {
 
         //this.getTask(this.appId,null,this.alarmStatus,this.page-1,this.pageMaxItem);
         //this.start_reply(reply);
-      } );*/
+      } );
 
   }
   start_reply(reply){
@@ -316,40 +243,6 @@ export class TaskManageComponent {
   getTask(id,name,status,page,size){
     this.offlineService.searchTask(id,name,status,page,size)
       .subscribe(result=>{
-        for(let i=0;i<result.content.length;i++){
-          if(result.content[i].taskName=='特定人识别-吴京'){
-            if(sessionStorage.getItem("特定人识别-吴京")){
-              let b = JSON.parse(sessionStorage.getItem("特定人识别-吴京"));
-              console.log(b);
-              result.content[i].percent=b.percent;
-              result.content[i].taskStatus=b.taskStatus;
-            }
-          }
-          if(result.content[i].taskName=='威胁度识别-枪械'){
-            if(sessionStorage.getItem("威胁度识别-枪械")){
-              let b = JSON.parse(sessionStorage.getItem("威胁度识别-枪械"));
-              console.log(b);
-              result.content[i].percent=b.percent;
-              result.content[i].taskStatus=b.taskStatus;
-            }
-          }
-          if(result.content[i].taskName=='作战目标识别-车+直升机'){
-            if(sessionStorage.getItem("作战目标识别-车+直升机")){
-              let b = JSON.parse(sessionStorage.getItem("作战目标识别-车+直升机"));
-              console.log(b);
-              result.content[i].percent=b.percent;
-              result.content[i].taskStatus=b.taskStatus;
-            }
-          }
-          if(result.content[i].taskName=='作战目标识别-车+直升机+坦克'){
-            if(sessionStorage.getItem("作战目标识别-车+直升机+坦克")){
-              let b = JSON.parse(sessionStorage.getItem("作战目标识别-车+直升机+坦克"));
-              console.log(b);
-              result.content[i].percent=b.percent;
-              result.content[i].taskStatus=b.taskStatus;
-            }
-          }
-        }
         this.taskList = result.content;
 /*        for(let i=0;i<this.taskList.length;i++){
           if(this.taskList[i].taskStatus!='进行中'){
