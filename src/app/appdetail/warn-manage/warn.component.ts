8885554450a0537d1713f2b22cb6ef7f58990350
fengtangzheng <tangzheng.feng@qq.com>
2018-04-20 16:38:49 +0800

    file download

--- src/app/appdetail/warn-manage/warn.component.ts
+++ src/app/appdetail/warn-manage/warn.component.ts
@@ -775,8 +775,7 @@ document.getElementById("image")
 
   downloadFile(item) {
     let path = item.imagePath;
-    let alarmId = item.alarmId;
-    this.warnService.downloadFile(alarmId, path).subscribe(data => {
+    this.warnService.downloadFile(path).subscribe(data => {
       var name = this.getDateFormat() + item.frameNo + item.alarmRule.recognitionCategory.name;
       var tempPathArr = path.split(".");
       var suffix = tempPathArr[tempPathArr.length - 1];
